376dc3cc5b242aed8e62edfb1840a0719b9880ba
learner <roadoflearning@live.com>
2017-02-07 13:53:17 +0800

    init project

---
+++ app/src/main/java/cn/zheteng123/m_volunteer/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.zheteng123.m_volunteer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
