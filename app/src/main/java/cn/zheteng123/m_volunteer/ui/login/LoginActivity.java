e2532e880a6f90d6ce04711a7ce8e627241fbddf
learner <roadoflearning@live.com>
2017-02-12 17:26:41 +0800

    更改配色，完成登录界面UI

--- app/src/main/java/cn/zheteng123/m_volunteer/ui/login/LoginActivity.java
+++ app/src/main/java/cn/zheteng123/m_volunteer/ui/login/LoginActivity.java
@@ -1,7 +1,10 @@
 package cn.zheteng123.m_volunteer.ui.login;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import butterknife.ButterKnife;
 import cn.zheteng123.m_volunteer.R;
@@ -11,7 +14,17 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_login);
+
 
         ButterKnife.bind(this);
     }
