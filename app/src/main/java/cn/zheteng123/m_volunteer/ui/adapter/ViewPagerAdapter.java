ce91ad750955124cffc022748f3b01b667819f66
learner <roadoflearning@live.com>
2017-02-16 15:28:08 +0800

    为 MainActivity 添加 ViewPager 布局，便于测试 fragment

---
+++ app/src/main/java/cn/zheteng123/m_volunteer/ui/adapter/ViewPagerAdapter.java
@@ -0,0 +1,32 @@
+package cn.zheteng123.m_volunteer.ui.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created on 2017/2/12.
+ */
+
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> mFragmentList;
+
+    public ViewPagerAdapter(FragmentManager fm, List<Fragment> fragmentList) {
+        super(fm);
+        mFragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
---
+++ app/src/main/java/cn/zheteng123/m_volunteer/ui/main/MainActivity.java
@@ -0,0 +1,48 @@
+package cn.zheteng123.m_volunteer.ui.main;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.zheteng123.m_volunteer.R;
+import cn.zheteng123.m_volunteer.ui.home.HomeFragment;
+import cn.zheteng123.m_volunteer.ui.adapter.ViewPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+
+    private List<Fragment> mFragmentList = new ArrayList<>();
+    private HomeFragment mHomeFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        setContentView(R.layout.activity_main);
+
+        mHomeFragment = new HomeFragment();
+        mFragmentList.add(mHomeFragment);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), mFragmentList);
+
+        mViewPager = (ViewPager) findViewById(R.id.vp_page);
+        mViewPager.setAdapter(viewPagerAdapter);
+
+    }
+}
