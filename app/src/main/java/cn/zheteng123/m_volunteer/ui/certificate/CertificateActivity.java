f068bd79c4ace4bc38b7ad20b65829d3b0b79339
learner <roadoflearning@live.com>
2017-02-25 20:00:40 +0800

    证书申请界面透明状态栏

--- app/src/main/java/cn/zheteng123/m_volunteer/ui/certificate/CertificateActivity.java
+++ app/src/main/java/cn/zheteng123/m_volunteer/ui/certificate/CertificateActivity.java
@@ -2,6 +2,8 @@ package cn.zheteng123.m_volunteer.ui.certificate;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -41,6 +43,14 @@ public class CertificateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_certificate);
         ButterKnife.bind(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 
         initListener();
     }
